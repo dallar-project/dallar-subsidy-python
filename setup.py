95bd6e6ae47b89cb0d01a8fd32efc43e5d4074e5
Michael Allar <allar@michaelallar.com>
2017-12-28 22:17:40 -0800

    Pip me up captain

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from distutils.core import setup, Extension
 dallar_subsidy_module = Extension('dallar_subsidy', sources = ['dallar_subsidy.cpp'])
 
 setup (name = 'dallar_subsidy',
-       version = '1.0',
-       description = 'Subsidy function for Dallar',
-       ext_modules = [dallar_subsidy_module])
+    version = '1.0',
+    description = 'Subsidy function for Dallar',
+    maintainer = 'Allar',
+    maintainer_email = 'allar@gamemak.in',
+    url = 'https://github.com/dallar-project/dallar-subsidy-python',
+    keywords = ['dallar', 'dal', 'throestl'],
+    ext_modules = [dallar_subsidy_module])
